3d9e5a85d1972b0a176ed189d3d9730a64cf118c
Wolf McNally <wolf@wolfmcnally.com>
2023-05-22 23:57:58 -0700

    Working.

--- src/ur_decodable.rs
+++ src/ur_decodable.rs
@@ -4,28 +4,6 @@ use crate::{ur::UR, error::Error};
 
 use dcbor::CBORTaggedDecodable;
 
-/*
-```swift
-import Foundation
-import DCBOR
-
-public protocol URDecodable: CBORTaggedDecodable {
-    init(ur: UR) throws
-}
-
-public extension URDecodable {
-    init(ur: UR) throws {
-        try ur.checkType(Self.cborTag.name!)
-        try self.init(untaggedCBOR: ur.cbor)
-    }
-
-    init(urString: String) throws {
-        try self.init(ur: UR(urString: urString))
-    }
-}
-```
- */
-
 pub trait URDecodable: CBORTaggedDecodable {
     fn from_ur(ur: &UR) -> Result<Rc<Self>, Error> {
         ur.check_type(&Self::CBOR_TAG.name().as_ref().unwrap())?;

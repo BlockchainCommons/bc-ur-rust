4f46e7f606a0dd02d40e8ab998330338559cff8d
Wolf McNally <wolf@wolfmcnally.com>
2023-09-25 01:52:49 -0700

    Bumped version number.

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ This crate does not currenly provide opinionated affordances for multi-part URs 
 
 ```toml
 [dependencies]
-bc-ur = "0.1.7"
+bc-ur = "0.2.0"
 ```
 
 ## Specification
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/bc-ur/0.1.7")]
+#![doc(html_root_url = "https://docs.rs/bc-ur/0.2.0")]
 #![warn(rust_2018_idioms)]
 
 //! # Blockchain Commons Uniform Resources ("UR") for Rust
@@ -32,7 +32,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! bc-ur = "0.1.7"
+//! bc-ur = "0.2.0"
 //! ```
 //!
 //! # Specification

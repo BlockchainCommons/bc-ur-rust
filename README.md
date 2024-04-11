4d0beb810ed529ee9962efaf16b0fe5364cacd19
Wolf McNally <wolf@wolfmcnally.com>
2024-04-10 23:41:26 -0700

    Aligned to dependencies.

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ This crate does not currenly provide opinionated affordances for multi-part URs 
 
 ```toml
 [dependencies]
-bc-ur = "0.3.0"
+bc-ur = "0.3.1"
 ```
 
 ## Specification
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/bc-ur/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/bc-ur/0.3.1")]
 #![warn(rust_2018_idioms)]
 
 //! # Blockchain Commons Uniform Resources ("UR") for Rust
@@ -32,7 +32,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! bc-ur = "0.3.0"
+//! bc-ur = "0.3.1"
 //! ```
 //!
 //! # Specification
